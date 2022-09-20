170546c476c1b1575038401f9f3446e58a5c04ef
Henning Kuenne <henning.kuenne@tudock.de>
2022-09-20 09:57:58 +0200

    fix indents

--- js/Components/Category/List.js
+++ js/Components/Category/List.js
@@ -4,76 +4,76 @@ const ITEMS_COUNT = 24;
 const ITEMS_URL = "https://dummyjson.com/products";
 
 class List extends AbstractComponent {
-  get elements() {
-    return {
-      items: ".items",
-    };
-  }
-
-  async connectedCallback() {
-    console.time("Category");
-    super.connectedCallback();
-    if (this.dataset.pwa === "true") {
-      try {
-        const items = await this.load();
-        const nodes = this.initProducts(items);
-        this.render(nodes);
-      } catch (error) {
-        console.error(error);
-      }
+    get elements() {
+        return {
+            items: ".items",
+        };
     }
-    console.timeEnd("Category");
-  }
 
-  async load() {
-    console.groupCollapsed("Start load timer.");
-    console.time("load");
-    const response = await fetch(ITEMS_URL);
-    const result = await response.json();
-    const items = result?.products?.slice(0, ITEMS_COUNT);
-    console.timeEnd("load");
-    console.groupEnd();
-    return items;
-  }
+    async connectedCallback() {
+        console.time("Category");
+        super.connectedCallback();
+        if (this.dataset.pwa === "true") {
+            try {
+                const items = await this.load();
+                const nodes = this.initProducts(items);
+                this.render(nodes);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        console.timeEnd("Category");
+    }
 
-  initProducts(items) {
-    console.groupCollapsed("Creating product tiles...");
-    console.time("initProducts");
-    const nodes = [];
-    items.forEach((p) => {
-      const node = document.createElement("category-item");
-      node.productData = p;
-      nodes.push(node);
-    });
-    console.log(`...${ITEMS_COUNT} product tiles created`);
-    console.timeEnd("initProducts");
-    console.groupEnd();
-    return nodes;
-  }
+    async load() {
+        console.groupCollapsed("Start load timer.");
+        console.time("load");
+        const response = await fetch(ITEMS_URL);
+        const result = await response.json();
+        const items = result?.products?.slice(0, ITEMS_COUNT);
+        console.timeEnd("load");
+        console.groupEnd();
+        return items;
+    }
 
-  /**
-   * Append items and assign to slot
-   * @param {Array.<HTMLElement>} nodes
-   */
-  render(nodes) {
-    console.groupCollapsed("Render tiles");
-    console.time("render");
-    const slot = this.shadow.querySelector("slot");
-    nodes.forEach((p) => {
-      this.appendChild(p);
-      slot.assign(p);
-    });
-    console.log(`${ITEMS_COUNT} product tiles rendered`);
-    console.timeEnd("render");
-    console.groupEnd();
-  }
+    initProducts(items) {
+        console.groupCollapsed("Creating product tiles...");
+        console.time("initProducts");
+        const nodes = [];
+        items.forEach((p) => {
+            const node = document.createElement("category-item");
+            node.productData = p;
+            nodes.push(node);
+        });
+        console.log(`...${ITEMS_COUNT} product tiles created`);
+        console.timeEnd("initProducts");
+        console.groupEnd();
+        return nodes;
+    }
 
-  /**
-   * Obtain category-item's from slot
-   */
-  get products() {
-    return this.items.querySelector("slot").assignedElements();
-  }
+    /**
+     * Append items and assign to slot
+     * @param {Array.<HTMLElement>} nodes
+     */
+    render(nodes) {
+        console.groupCollapsed("Render tiles");
+        console.time("render");
+        const sl = this.shadow.querySelector("slot");
+        nodes.forEach((p) => {
+            this.appendChild(p);
+            sl.assign(p);
+        });
+        console.log(`${ITEMS_COUNT} product tiles rendered`);
+        console.timeEnd("render");
+        console.groupEnd();
+    }
+
+    /**
+     * Obtain category-item's from slot
+     */
+    get products() {
+        return this.items.querySelector("slot").assignedElements();
+    }
 }
 
 const template = /* html */ `
