6dc71016b851ef6311bd43905025c5b3f5d6faee
Henning Kuenne <henning.kuenne@tudock.de>
2022-09-22 09:38:57 +0200

    add accordeon

---
+++ js/Components/Elements/Accordeon/Item.js
@@ -0,0 +1,138 @@
+import { AbstractComponent } from "../../Abstract.js";
+
+class AccordeonItem extends AbstractComponent {
+    static observedAttributes = ["data-open"];
+
+    /** @inheritdoc */
+    get elements() {
+        return {
+            contentSlot: "div.content slot",
+            contentNode: "div.content",
+        };
+    }
+
+    /** @inheritdoc */
+    get listeners() {
+        return {
+            "div.label": {
+                click: this.toggle.bind(this),
+            },
+        };
+    }
+
+    /**
+     * Handle render
+     */
+    connectedCallback() {
+        if (!this.classList.contains("clone")) {
+            super.connectedCallback();
+            this.dataset.open = this.dataset.open ?? false.toString();
+            this.initClone();
+        }
+    }
+
+    /**
+     * clone self and hide in open state
+     * for measuring height
+     */
+    initClone() {
+        this.clone = new this.constructor();
+        this.clone.innerHTML = this.innerHTML;
+        this.clone.classList.add("clone");
+        this.parentNode.appendChild(this.clone);
+        requestAnimationFrame(() => {
+            this.style.setProperty(
+                "--height",
+                this.clone.contentNode.offsetHeight
+            );
+        });
+    }
+
+    /**
+     * Handle attribute change
+     * @param {String} name
+     * @param {String} oldValue
+     * @param {String} newValue
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (!this.isConnected || oldValue === newValue) return;
+        if ("data-open" === name) {
+            this.open = newValue === "true";
+            this.dispatchEvent(new CustomEvent("change", { detail: this }));
+        }
+    }
+
+    /**
+     * trigger height calculation
+     */
+    update() {
+        if (this.open) {
+            this.style.setProperty(
+                "--height",
+                this.clone.contentNode.offsetHeight
+            );
+        }
+    }
+
+    /**
+     * Toggle open state
+     */
+    toggle() {
+        this.open = !this.open;
+        this.update();
+    }
+
+    /**
+     * Obtain open state
+     */
+    get open() {
+        return this.dataset.open === "true";
+    }
+
+    /**
+     * Set open state
+     * @param {Boolean} v
+     */
+    set open(v) {
+        if (this.open !== !!v) {
+            this.dataset.open = (!!v).toString();
+        }
+    }
+}
+
+const template = /* html */ `
+<style>
+:host {
+    box-sizing: border-box;
+    display: block;
+    overflow: hidden;
+    position: relative;
+    isolation: isolate;
+}
+div.label {
+    cursor: pointer;
+}
+:host(:not(.clone)) div.content {
+    max-height: 0;
+}
+div.content,
+div.content slot::slotted(*) {
+    transition: all var(--transition-duration, 250ms) var(--transition-timing-function, ease-in-out);
+}
+:host(:not(.clone)[data-open="true"]) div.content {
+    max-height: calc(var(--height) * 1px);
+}
+:host(:not(.clone):not([data-open="true"])) div.content slot::slotted(*) {
+    margin-block: 0;
+}
+:host(.clone) {
+    position: absolute;
+    left: -200vw;
+    width: 100%;
+}
+</style>
+<div class="label"><slot name="label"></slot></div>
+<div class="content"><slot></slot></div>
+`;
+
+AbstractComponent.initComponent("x-accordeon-item", AccordeonItem, template);
---
+++ js/Components/Elements/Accordeon/index.js
@@ -0,0 +1,121 @@
+import { AbstractComponent } from "../../Abstract.js";
+import "./Item.js";
+
+/**
+ * @property {Array.<AccordeonItem>} items
+ */
+class Accordeon extends AbstractComponent {
+    /** @inheritdoc */
+    get elements() {
+        return {
+            itemSlot: "slot",
+        };
+    }
+
+    /**
+     * Initialize
+     */
+    connectedCallback() {
+        super.connectedCallback();
+        const evtOptions = {
+            signal: this.disconnectedSignal,
+        };
+        window.addEventListener(
+            "resize",
+            () => this.updateItemList(),
+            evtOptions
+        );
+        this.itemSlot.addEventListener(
+            "slotchange",
+            () => this.updateItemList(0),
+            evtOptions
+        );
+        if (!screen.orientation) {
+            window.addEventListener(
+                "orientationchange",
+                () => this.updateItemList(),
+                evtOptions
+            );
+        } else {
+            screen.orientation.addEventListener(
+                "change",
+                () => this.updateItemList(),
+                evtOptions
+            );
+        }
+        this.itemChangeHandler = this.handleItemChange.bind(this);
+        this.items.forEach((i) =>
+            i.addEventListener("change", this.itemChangeHandler, evtOptions)
+        );
+    }
+
+    /**
+     * Handle item open state change
+     *
+     * @param {CustomEvent} e
+     */
+    handleItemChange(e) {
+        this.items.forEach((i) =>
+            i.removeEventListener("change", this.itemChangeHandler)
+        );
+        if (e.detail.open && !this.multi) {
+            this.items
+                ?.filter((i) => i !== e.detail)
+                .forEach((i) => (i.open = false));
+        }
+        this.items.forEach((i) =>
+            i.addEventListener("change", this.itemChangeHandler, {
+                signal: this.disconnectedSignal,
+            })
+        );
+    }
+
+    /**
+     * Update list of items
+     *
+     * @param {Number} debounce
+     */
+    updateItemList(debounce = null) {
+        const timeout = debounce ?? 50;
+        if (this.debounceTimeout) {
+            clearTimeout(this.debounceTimeout);
+        }
+        this.debounceTimeout = setTimeout(() => {
+            this.items.forEach((i) => i.update());
+        }, timeout);
+    }
+
+    /**
+     * Close all items
+     */
+    closeAll() {
+        this.items.forEach((i) => (i.open = false));
+    }
+
+    /**
+     * obtain items
+     */
+    get items() {
+        return Array.from(
+            this.querySelectorAll("x-accordeon-item:not(.clone)")
+        );
+    }
+
+    /**
+     * Determine if accordeon can open multiple tabs
+     */
+    get multi() {
+        return "undefined" !== typeof this.dataset.multi;
+    }
+}
+
+const template = /* html */ `
+<style>
+:host {
+    position: relative;
+}
+</style>
+<slot></slot>
+`;
+
+AbstractComponent.initComponent("x-accordeon", Accordeon, template);
